8ba0e4776dc6f8d85e9f8e01c3bb60a8fe99b76d
nabilrobbani84 <nabilrobbani84@gmail.com>
2026-05-19 19:27:32 +0700

    Ensure Vercel runtime view directory exists

--- api/index.php
+++ api/index.php
@@ -2,4 +2,14 @@
 
 use Illuminate\Http\Request;
 
+$runtimeViewPath = getenv('VIEW_COMPILED_PATH') ?: ($_SERVER['VIEW_COMPILED_PATH'] ?? $_ENV['VIEW_COMPILED_PATH'] ?? '/tmp/views');
+
+if (is_string($runtimeViewPath)) {
+    $runtimeViewPath = trim($runtimeViewPath, "\"' ");
+
+    if ($runtimeViewPath !== '' && ! is_dir($runtimeViewPath)) {
+        mkdir($runtimeViewPath, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
